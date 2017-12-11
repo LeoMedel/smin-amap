1cfbab7af241306115e3e1deb844cbcc55af3f07
LeoMedel <leonel.ilhuicatzi07@gmail.com>
2017-12-11 08:53:06 -0600

    first commit

--- amap/src/index.js
+++ amap/src/index.js
@@ -1,1 +1,16 @@
 // c'est parti !
+import React from 'react';
+import { render } from 'react-dom';
+
+
+class StorePicker extends React.Component
+{
+	render(){
+		return <p> Hello ! </p>
+	}
+}
+
+
+render(<StorePicker/>, document.querySelector('#main'));
+
+
